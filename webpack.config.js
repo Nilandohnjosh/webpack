b9660e8cc83e4178d68bc040e8dfb4e9ed2cfe7b
Nila <26017191+Nilandohnjosh@users.noreply.github.com>
2022-06-18 13:58:52 +1200

    use source maps to pinpoint error file, and setting up development tools. made sc ripts in package.json to automate changes

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,18 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+  mode: 'development',
   entry: {
     index: './src/index.js',
     print: './src/print.js',
   },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
+      title: 'Development',
     }),
   ],
   output: {
@@ -16,5 +21,9 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+    publicPath: '/',
+  },
+  optimization: {
+    runtimeChunk: 'single',
   },
 }
